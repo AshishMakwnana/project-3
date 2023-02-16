1b7d5b4852155cb0b926422c95b25376754b54cd
Ashish Makwana <ashishmakwana435@gmail.com>
2023-02-16 19:15:08 +0530

    hook example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  const puple = "#8e44ad";
+  const [bg_color , setBg] = useState(puple);
+  const [name,setName] = useState("Click ME !")
+
+  const BgChange = ()=>{
+    const newBg = "#313131";
+    setBg(newBg);
+    setName("❤ yap ❤")
+  };
+  const dbChange = () =>{
+      setBg(puple);
+      setName("one more");
+  }
+    return(
+    <>
+      <div style={{backgroundColor:bg_color}} className='App-header'>
+        <button className='btn' onMouseEnter={BgChange} onMouseLeave={dbChange}>{name}</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
