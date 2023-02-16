5a5715b2e0ce9f1fcfe8ed3a102649ed712c60c7
Ashish Makwana <ashishmakwana435@gmail.com>
2023-02-16 17:08:49 +0530

    inc_dec_value app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+const App = () =>{
+  const state = useState();
+  const [count,setCount] = useState(0)
+  const IncVal =()=>{
+    setCount(count+1);
+  };
+  
+  const DecVal =() =>{
+    setCount(count-1);
+  };
+  return (<>
+  <div className="main">
+    <h1 className="heading__style">{count}</h1>
+    <div className="btns">
+    <button className="inc_btn btn" onClick={IncVal}>➕</button>
+    <button className="dec_btn btn" onClick={DecVal}>➖</button>
     </div>
+  </div>
+  </>
   );
-}
+};
 
 export default App;
