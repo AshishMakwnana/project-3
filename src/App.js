d488a27ef19d576d7fb0325ae70070737ed1bf5f
Ashish Makwana <ashishmakwana435@gmail.com>
2023-02-20 01:27:00 +0530

    create a simple form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [FullName, setName] = useState({
+    Fname: "",
+    Lname: "",
+    email :"",
+    phone :""
+  });
+
+  const InpHandler = (event) => {
+    // fetch the value of inputs tag
+    const {value, name} = event.target;
+    
+    setName((preValue)=>{
+      return{
+        ...preValue,
+      [name]:value,
+      }
+    });
+
+    // check the value of name of input 
+    // if (name === "Fname") {
+    //   setName((preValue) => {
+    //     return {
+    //       fname: value,
+    //       lname: preValue.lname,
+    //       email: preValue.email,
+    //       phone : preValue.phone
+    //     };
+    //   });
+    // } else if (name === "Lname") {
+    //   setName((preValue) => {
+    //     return {
+    //       lname: value,
+    //       fname: preValue.fname,
+    //       email: preValue.email,
+    //       phone : preValue.phone
+    //     };
+    //   });
+    // } else if (name === "email") {
+    //   setName((preValue) => {
+    //     return {
+    //       lname: preValue.lname,
+    //       fname: preValue.fname,
+    //       email: value,
+    //       phone : preValue.phone
+    //     };
+    //   });
+    // } else if (name === "phone") {
+    //   setName((preValue) => {
+    //     return {
+    //       lname: preValue.value,
+    //       fname: preValue.fname,
+    //       email: preValue.email,
+    //       phone: value
+    //     };
+    //   });
+    // };
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    alert("Form Submited")
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App-header">
+        <div className="container">
+          <h1>Hello {FullName.Fname} {FullName.Lname}</h1>
+          <p>{FullName.email}</p>
+          <p>{FullName.phone}</p>
+          <form onSubmit={submit}>
+            <input type="text" name="Fname" placeholder="Enter your first name" onChange={InpHandler} value={FullName.value} />
+            <input type="text" name="Lname" placeholder="Enter your Last name" onChange={InpHandler} value={FullName.value} />
+            <input type="email" name="email" placeholder="Enter your email" onChange={InpHandler} value ={FullName.email} />
+            <input type="number" name="phone" placeholder="Enter your Phone Number" onChange={InpHandler} value = {FullName.phone} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
